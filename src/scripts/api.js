009f7ab66952fc3080eb25df8e3da629c1097db4
David Hilera <david.hilera@hotmail.com>
2022-12-10 07:46:25 -0600

    [config] Add linter, folders & gitignore
    What changed?
      - Config Linter with Eslint.
      - Change order of folder structure.
      - Add basic .gitignore.
    
    Because change?
      - Proyect configurations.

---
+++ src/scripts/api.js
@@ -0,0 +1,13 @@
+const urlApi = 'https://rickandmortyapi.com/api/character';
+
+async function getData() {
+  const response = await fetch(urlApi)
+    .then((response) => {
+      return response.json();
+    });
+  const data = response;
+  console.log(data);
+  return data;
+}
+
+getData();
